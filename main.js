9acae2fe4bd88b10cd7a96ef8873a51721c2ade9
Reboot Student <didivergente@gmail.com>
2024-10-01 15:48:48 +0100

    Fixed merge issues

--- main.js
+++ main.js
@@ -1,162 +1,143 @@
-let boardGame = document.getElementById("boardGame")
-let startView = document.getElementById('start')
-let restartView = document.getElementById('restart')
-let score = document.getElementById('score')
+let boardGame = document.getElementById("boardGame");
+let startView = document.getElementById("start");
+let restartView = document.getElementById("restart");
+let score = document.getElementById("score");
 
-let heartCounter = document.getElementById('health')
+let heartCounter = document.getElementById("health");
 
+let buttonStart = document.getElementById("btn-start");
+let buttonEnd = document.getElementById("btn-end");
 
-let buttonStart = document.getElementById('btn-start')
-let buttonEnd = document.getElementById('btn-end')
+let rebootnator;
+let moveRebootnatorInterval;
 
+let enemy;
+let enemiesSpawnInterval;
 
-let rebootnator
-let moveRebootnatorInterval
+let goodie;
+let enemies = [];
+let goodies = [];
+let goodieSpawnInterval;
 
+let handsUp;
 
-let enemy
-let enemiesSpawnInterval
-
-let goodie
-let enemies = []
-let goodies = []
-let goodieSpawnInterval
-
-let handsUp
-    
 function startGame() {
-    newPlayer()
-    newEnemies()
-    newGoodies()
+  newPlayer();
+  newEnemies();
+  newGoodies();
 }
 
+function gameWorking() {
+  if (rebootnator.lives > 0) {
+    rebootnator.move();
+    console.log("asdasd");
+  } else {
+    endGame();
+  }
+}
 
-function gameWorking (){
-    
-    if (rebootnator.lives > 0){
-        rebootnator.move()
-        console.log('asdasd')
-        
-    } else { 
-        endGame() } 
-    }
-
-    
-    
 function newPlayer() {
-    rebootnator = new Rebootnator(300, 700)
-    rebootnator.insert()
-    moveRebootnatorInterval = setInterval(function () {
-    gameWorking()
-    }, 5)
+  rebootnator = new Rebootnator(300, 700);
+  rebootnator.insert();
+  moveRebootnatorInterval = setInterval(function () {
+    gameWorking();
+  }, 5);
 }
-    
-    function newEnemies() {
-        enemiesSpawnInterval = setInterval(function () {
-            enemy = new Enemy()
-            /*
+
+function newEnemies() {
+  enemiesSpawnInterval = setInterval(function () {
+    enemy = new Enemy();
+    /*
             const enemyTypes = ['js', 'html', 'css']
             const decider = enemyTypes[Math.floor(Math.random() * enemyTypes)] 
             */
-            enemy.insert()
-            enemies.push(enemy)
-    }, 500)
+    enemy.insert();
+    enemies.push(enemy);
+  }, 500);
 }
 
 function newGoodies() {
-    goodieSpawnInterval = setInterval(function () {
-        const types = ['slimSalad', 'speedCoffee', 'thickBurger']
-        const randomType = types[Math.floor(Math.random() * types.length)] 
-        goodie = new Goodies(randomType)
-        goodie.insert()
-        goodies.push(goodie)
-    }, 6000)
-
-
+  goodieSpawnInterval = setInterval(function () {
+    const types = ["slimSalad", "speedCoffee", "thickBurger"];
+    const randomType = types[Math.floor(Math.random() * types.length)];
+    goodie = new Goodies(randomType);
+    goodie.insert();
+    goodies.push(goodie);
+  }, 6000);
 }
 
 function fireHandsUp() {
-    handsUp = new HandsUp()
-    handsUp.insert()
+  handsUp = new HandsUp();
+  handsUp.insert();
 }
 
-window.addEventListener('keydown', function (event) {
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "a":
+      rebootnator.directionX = -1;
+      rebootnator.move();
+      break;
 
-    switch (event.key) {
+    case "d":
+      rebootnator.directionX = 1;
+      rebootnator.move();
+      break;
 
-        case 'a':
-            rebootnator.directionX = -1
-            rebootnator.move()
-            break;
+    case "A":
+      rebootnator.directionX = -1;
+      rebootnator.move();
+      break;
 
-        case 'd':
-            rebootnator.directionX = 1
-            rebootnator.move()
-            break;
+    case "D":
+      rebootnator.directionX = 1;
+      rebootnator.move();
+      break;
 
-        case 'A':
-            rebootnator.directionX = -1
-            rebootnator.move()
-            break;
+    case "ArrowLeft":
+      rebootnator.directionX = -1;
+      rebootnator.move();
+      break;
 
-        case 'D':
-            rebootnator.directionX = 1
-            rebootnator.move()
-            break;
+    case "ArrowRight":
+      rebootnator.directionX = 1;
+      rebootnator.move();
+      break;
 
-        case 'ArrowLeft':
-            rebootnator.directionX = -1
-            rebootnator.move()
-            break;
+    case " ":
+      fireHandsUp();
+      break;
+  }
+});
 
-        case 'ArrowRight':
-            rebootnator.directionX = 1
-            rebootnator.move()
-            break;
+window.addEventListener("keyup", function (event) {
+  rebootnator.directionX = 0;
+});
 
+buttonStart.addEventListener("click", function (event) {
+  startGame();
+});
 
-        case ' ':
-            fireHandsUp()
-            break;
+buttonEnd.addEventListener("click", function (event) {
+  endGame();
+});
 
-    }
-})
+function endGame() {
+  clearInterval(moveRebootnatorInterval);
+  clearInterval(goodieSpawnInterval);
+  clearInterval(enemiesSpawnInterval);
 
-window.addEventListener('keyup', function (event) {
-    rebootnator.directionX = 0
-})
+  rebootnator.remove();
 
+  enemies.forEach(function (enemy, index) {
+    enemy.remove();
+  });
 
-buttonStart.addEventListener('click' , function (event) {
-    startGame()
-})
+  goodies.forEach(function (goodie, index) {
+    goodie.remove();
+  });
 
-buttonEnd.addEventListener('click', function (event) {
-    endGame()
-})
+  goodies = [];
+  enemies = [];
 
-
-
-function endGame () {
-    clearInterval(moveRebootnatorInterval)
-    clearInterval(goodieSpawnInterval)
-    clearInterval(enemiesSpawnInterval)
-    
-    rebootnator.remove()
-
-    enemies.forEach(function(enemy,index){
-        enemy.remove()
-    })
-
-    goodies.forEach(function(goodie, index){
-        goodie.remove()
-    })
-
-    goodies = []
-    enemies = []
-
-    console.log('FIN DE JUEGO')
-    
+  console.log("FIN DE JUEGO");
 }
-
-
